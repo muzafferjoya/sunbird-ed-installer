47ab6344989b541d05df33932bb62349b971dd5b
Muzaffar khan <muzafferjoya@gmail.com>
2024-03-02 18:00:19 +0000

    updated

--- terraform/azure/modules/keys/main.tf
+++ terraform/azure/modules/keys/main.tf
@@ -1,7 +1,7 @@
 provider "null" {}
 
 locals {
-  global_values_keys_file         = "${var.base_location}/../global-keys-values.yaml"
+  global_values_keys_file          = "${var.base_location}/../global-keys-values.yaml"
   jwt_script_location             = "${var.base_location}/../../../../scripts/jwt-keys.py"
   rsa_script_location             = "${var.base_location}/../../../../scripts/rsa-keys.py"
   global_values_jwt_file_location = "${var.base_location}/../../../../scripts/global-values-jwt-tokens.yaml"
@@ -12,15 +12,13 @@ resource "null_resource" "generate_jwt_keys" {
   triggers = {
     command = "${timestamp()}"
   }
+
   provisioner "local-exec" {
     command = "python3 ${local.jwt_script_location} ${var.random_string} && cp ${local.global_values_jwt_file_location} ${var.base_location}/../global-values-jwt-tokens.yaml"
   }
 }
 
 resource "null_resource" "generate_rsa_keys" {
-  triggers = {
-    command = "${timestamp()}"
-  }
   provisioner "local-exec" {
     command = "python3 ${local.rsa_script_location} ${var.rsa_keys_count} && cp ${local.global_values_rsa_file_location} ${var.base_location}/../global-values-rsa-keys.yaml"
   }
@@ -30,9 +28,11 @@ resource "null_resource" "upload_global_jwt_values_yaml" {
   triggers = {
     command = "${timestamp()}"
   }
+
   provisioner "local-exec" {
-    command = "aws s3api put-object --bucket ${var.storage_account_name} --key ${var.environment}-global-values-jwt-tokens.yaml --body ${var.base_location}/../global-values-jwt-tokens.yaml"
+    command = "aws s3 cp ${var.base_location}/../global-values-jwt-tokens.yaml s3://${var.storage_bucket_name}/${var.environment}-global-values-jwt-tokens.yaml"
   }
+
   depends_on = [null_resource.generate_jwt_keys]
 }
 
@@ -40,8 +40,10 @@ resource "null_resource" "upload_global_rsa_values_yaml" {
   triggers = {
     command = "${timestamp()}"
   }
+
   provisioner "local-exec" {
-    command = "aws s3api put-object --bucket ${var.storage_account_name} --key ${var.environment}-global-values-rsa-keys.yaml --body ${var.base_location}/../global-values-rsa-keys.yaml"
+    command = "aws s3 cp ${var.base_location}/../global-values-rsa-keys.yaml s3://${var.storage_bucket_name}/${var.environment}-global-values-rsa-keys.yaml"
   }
+
   depends_on = [null_resource.generate_rsa_keys]
 }
--- terraform/azure/modules/keys/variables.tf
+++ terraform/azure/modules/keys/variables.tf
@@ -8,42 +8,80 @@ variable "building_block" {
     description = "Building block name. All resources will be prefixed with this value."
 }
 
-variable "storage_account_name" {
+variable "bucket_name" {
     type        = string
-    description = "Storage account name."
+    description = "Bucket name for S3 storage."
 }
 
-variable "storage_container_public" {
+variable "public_bucket_name" {
     type        = string
-    description = "Public storage container name with blob access."
-}
-
-variable "storage_container_private" {
-    type        = string
-    description = "Private storage container name."
-}
-
-variable "storage_account_primary_access_key" {
-    type        = string
-    description = "Storage account primary access key."
-}
-
-variable "base_location" {
-    type        = string
-    description = "Location of terrafrom execution folder."
+    description = "Public bucket name with blob access."
 }
 
 variable "rsa_keys_count" {
     type        = number
-    description = "Number of rsa keys to generate"
+    description = "Number of RSA keys to generate"
     default     = 2
 }
 
- variable "random_string" {
+variable "random_string" {
     type        = string
     description = "This string will be used to encrypt / mask various values. Use a strong random string in order to secure the applications. The string should be between 12 and 24 characters in length. If you forget the string, the application will stop working and the string cannot be retrieved."
     validation {
       condition     = length(var.random_string) >= 12 || length(var.random_string) <= 24
       error_message = "The string must have a length ranging from 12 to 24 characters."
   }
+}
+
+# provider "aws" {
+#     region = var.base_location
+# }
+
+provider "aws" {
+    region = "us-east-1" 
+}
+
+
+variable "base_location" {
+  type        = string
+  description = "Location of Terraform execution folder."
+  #default = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "storage_bucket" {
+    bucket = "${var.building_block}-${var.environment}-${var.bucket_name}"
+
+    tags = {
+        Environment   = var.environment
+        BuildingBlock = var.building_block
+    }
+}
+
+resource "aws_s3_bucket" "public_storage_bucket" {
+    bucket = "${var.building_block}-${var.environment}-${var.public_bucket_name}"
+
+    tags = {
+        Environment   = var.environment
+        BuildingBlock = var.building_block
+    }
+}
+
+resource "aws_s3_object" "private_objects" {
+    bucket = aws_s3_bucket.storage_bucket.id
+    key    = "private/"
+
+    server_side_encryption = "AES256"
+}
+
+resource "aws_s3_object" "public_objects" {
+    bucket = aws_s3_bucket.public_storage_bucket.id
+    key    = "public/"
+
+    acl    = "public-read"
+}
+
+variable "storage_bucket_name" {
+    type        = string
+    description = "Name of the S3 bucket where the file will be uploaded."
 }
